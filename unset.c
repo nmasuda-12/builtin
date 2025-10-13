1373987177ccf8e07018f09b48cb3d587e6103f1
Nanaho Masuda <nmasuda@c5r1s7.42tokyo.jp>
2025-10-13 18:38:51 +0900

    unset

--- unset.c
+++ unset.c
@@ -6,7 +6,7 @@
 /*   By: nmasuda <nmasuda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 17:46:10 by nmasuda           #+#    #+#             */
-/*   Updated: 2025/10/13 18:34:09 by nmasuda          ###   ########.fr       */
+/*   Updated: 2025/10/13 18:38:48 by nmasuda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,33 +77,6 @@ char	**c_unset(char **line, char **ev)
 	return (new_ev);
 }
 
-// char	**c_unset(char **line, char **ev)
-// {
-// 	char	**new_ev;
-// 	char	*new_line;
-// 	int		j;
-
-// 	j = 0;
-// 	while (ev[j])
-// 		j++;
-// 	new_ev = malloc(sizeof(char *) * (j + 1));
-// 	if (!new_ev)
-// 		error("unset_newev_malloc_error", NULL);
-// 	j = 0;
-// 	while (ev[j])
-// 	{
-// 		new_line = malloc(sizeof(char) * (ft_strlen(ev[j]) + 1));
-// 		if (!new_line)
-// 			error("unset_newline_malloc_error", new_ev);
-// 		ft_memmove(new_line, ev[j], ft_strlen(ev[j]));
-// 		new_ev[j] = new_line;
-// 		j++;
-// 	}
-// 	new_ev[j + 1] = "\0";
-// 	return (new_ev);
-// }
-
 /*
 ノーム直す
-あとなんか二番目の文字がうまく消えてくれない
 */
