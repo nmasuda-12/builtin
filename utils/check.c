6142f4a3b461a370951d2bafa58d548ecfaff1fe
Nanaho Masuda <nmasuda@c3r5s2.42tokyo.jp>
2025-10-16 21:23:45 +0900

    normちょっと直した

--- utils/check.c
+++ utils/check.c
@@ -6,18 +6,18 @@
 /*   By: nmasuda <nmasuda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 18:08:09 by nmasuda           #+#    #+#             */
-/*   Updated: 2025/10/15 18:18:44 by nmasuda          ###   ########.fr       */
+/*   Updated: 2025/10/16 21:22:55 by nmasuda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../builtin.h"
 
-void	c_check(char *bash,char **check)
+void	c_check(char *bash, char **check)
 {
 	int	i;
 
 	i = 0;
-	if(!check)
+	if (!check)
 		error(bash, NULL, NULL, 0);
 	while (check[i])
 		printf("%s\n", check[i++]);
